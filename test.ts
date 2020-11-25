ffd4d8c3fedaf50fcd90514511a5d24a38af254e
Chai Nunes <chaiyawahoo@gmail.com>
2020-11-24 17:06:28 -0800

    create particle emitter

--- test.ts
+++ test.ts
@@ -1,8 +1,9 @@
+import ParticleEmitter from './physics/particleEmitter.js'
 import Sprite from './physics/sprite.js';
 import Vector2 from './physics/vectors.js';
 let canvas = <HTMLCanvasElement> document.getElementById('test-canvas');
 let ctx = canvas.getContext('2d');
-let thing = new Sprite(
+let turtle = new Sprite(
   './src/images/turtle.png',
   new Vector2(50, 5),
   new Vector2(50, 50),
@@ -10,12 +11,28 @@ let thing = new Sprite(
   false,
   true
 );
-thing.applyForce(new Vector2(0,9.8));
+let fireball = new Sprite(
+  './src/images/fireball.png',
+  new Vector2(100, 50),
+  new Vector2(50, 50),
+  1,
+  false,
+  false
+);
+let emitter = new ParticleEmitter (
+  [turtle, fireball],
+  new Vector2(200, 200),
+  50,
+  10,
+  1
+);
+turtle.applyForce(new Vector2(0,9.8));
 setInterval(() => {
   //console.log(`V: ${thing.velocity.magnitude}\nA: ${thing.acceleration.magnitude}`);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  thing.render(ctx);
-  if (!thing.checkBounds(canvas)) {
-    thing.move(0.05);
+  emitter.update(ctx, 0.05);
+  turtle.render(ctx);
+  if (!turtle.checkBounds(canvas)) {
+    turtle.move(0.05);
   }
 }, 10);
