98d3c87c10457ae91eca8418d32bc27bb5c1e0c5
Chai Nunes <chaiyawahoo@gmail.com>
2020-11-24 20:37:38 -0800

    Revert "create particle emitter"
    This reverts commit ffd4d8c3fedaf50fcd90514511a5d24a38af254e.

--- Scripts/test.js
+++ Scripts/test.js
@@ -1,19 +1,15 @@
-import ParticleEmitter from './physics/particleEmitter.js';
 import Sprite from './physics/sprite.js';
 import Vector2 from './physics/vectors.js';
 var canvas = document.getElementById('test-canvas');
 var ctx = canvas.getContext('2d');
-var turtle = new Sprite('./src/images/turtle.png', new Vector2(50, 5), new Vector2(50, 50), 1, false, true);
-var fireball = new Sprite('./src/images/fireball.png', new Vector2(100, 50), new Vector2(50, 50), 1, false, false);
-var emitter = new ParticleEmitter([turtle, fireball], new Vector2(200, 200), 50, 10, 1);
-turtle.applyForce(new Vector2(0, 9.8));
+var thing = new Sprite('./src/images/turtle.png', new Vector2(50, 5), new Vector2(50, 50), 1, false, true);
+thing.applyForce(new Vector2(0, 9.8));
 setInterval(function () {
     //console.log(`V: ${thing.velocity.magnitude}\nA: ${thing.acceleration.magnitude}`);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    emitter.update(ctx, 0.05);
-    turtle.render(ctx);
-    if (!turtle.checkBounds(canvas)) {
-        turtle.move(0.05);
+    thing.render(ctx);
+    if (!thing.checkBounds(canvas)) {
+        thing.move(0.05);
     }
 }, 10);
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,9 +1,8 @@
-import ParticleEmitter from './physics/particleEmitter.js'
 import Sprite from './physics/sprite.js';
 import Vector2 from './physics/vectors.js';
 let canvas = <HTMLCanvasElement> document.getElementById('test-canvas');
 let ctx = canvas.getContext('2d');
-let turtle = new Sprite(
+let thing = new Sprite(
   './src/images/turtle.png',
   new Vector2(50, 5),
   new Vector2(50, 50),
@@ -11,28 +10,12 @@ let turtle = new Sprite(
   false,
   true
 );
-let fireball = new Sprite(
-  './src/images/fireball.png',
-  new Vector2(100, 50),
-  new Vector2(50, 50),
-  1,
-  false,
-  false
-);
-let emitter = new ParticleEmitter (
-  [turtle, fireball],
-  new Vector2(200, 200),
-  50,
-  10,
-  1
-);
-turtle.applyForce(new Vector2(0,9.8));
+thing.applyForce(new Vector2(0,9.8));
 setInterval(() => {
   //console.log(`V: ${thing.velocity.magnitude}\nA: ${thing.acceleration.magnitude}`);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  emitter.update(ctx, 0.05);
-  turtle.render(ctx);
-  if (!turtle.checkBounds(canvas)) {
-    turtle.move(0.05);
+  thing.render(ctx);
+  if (!thing.checkBounds(canvas)) {
+    thing.move(0.05);
   }
 }, 10);
